3ba6e7995fc4aa0c8f222a48f58d2426da48e4b2
IsaacIngram <isaac@isaacingram.com>
2022-12-01 00:57:32 -0500

    Working solution for day 1 part 1

---
+++ Day1/src/main.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+fn main() {
+
+    // Load the file
+    let file_path = "resources/input.txt";
+    let contents = fs::read_to_string(file_path).unwrap();
+
+    // An array of elves calorie amounts
+    let mut elves = Vec::new();
+
+    // The current sum to be added to by each elf
+    let mut current_sum: i32 = 0;
+
+    // Iterate through each line and add the calories to the array of elves
+    for line in contents.split("\n") {
+        if line != "" {
+            current_sum += line.parse::<i32>().unwrap();
+        } else {
+            // Add the current sum to the elves array and reset the current sum
+            elves.push(current_sum);
+            current_sum = 0;
+        }
+    }
+
+    // Sort the array
+    elves.sort();
+
+    // Get the greatest object
+    println!("{}",elves.last().unwrap());
+}
---
+++ Day1/target/debug/Day1.d
@@ -0,0 +1,1 @@
+/Users/isaacingram/Documents/Programming/AdventOfCode2022/Day1/target/debug/Day1: /Users/isaacingram/Documents/Programming/AdventOfCode2022/Day1/src/main.rs
