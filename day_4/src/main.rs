35f06c5843ae44594c3725c53f2ea80718909698
IsaacIngram <isaac@isaacingram.com>
2022-12-09 00:12:50 -0500

    Part 1

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,5 +1,33 @@
+use std::fs;
+
 fn main() {
 
+    // Load the file
+    let file_path = "input/sections.txt";
+    let contents = fs::read_to_string(file_path).unwrap();
 
+    // Sum of all assignments that are flawed
+    let mut flawed_assignments = 0;
 
+    // Iterate through all assignment pairs
+    for line in contents.split("\n") {
+        // Parse assignments
+        let separated_assignments: Vec<&str> = line.split(",").collect();
+        let assignment_1: Vec<&str> = separated_assignments[0].split("-").collect();
+        let assignment_2: Vec<&str> = separated_assignments[1].split("-").collect();
+        let assignment_1_start = assignment_1[0].parse::<i32>().unwrap();
+        let assignment_1_end = assignment_1[1].parse::<i32>().unwrap();
+        let assignment_2_start = assignment_2[0].parse::<i32>().unwrap();
+        let assignment_2_end = assignment_2[1].parse::<i32>().unwrap();
+
+        // Check if one range fully contains another
+        if (assignment_1_start <= assignment_2_start && assignment_1_end >= assignment_2_end) ||
+            (assignment_2_start <= assignment_1_start && assignment_2_end >= assignment_1_end) {
+            // Increase the number of flawed assignments
+            flawed_assignments += 1;
+        }
+    }
+
+    // Print output
+    println!("Part 1 solution: {}", flawed_assignments);
 }
